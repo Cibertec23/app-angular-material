1ac065e66451310d5e1dc91de8f4e3beeab020f2
cuartociclo2023 <cuartocicloidat2023@gmail.com>
2023-11-11 20:27:40 -0500

    directivas y pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DirectivasPipesComponent } from './ejemplos/directivas-pipes/directivas-pipes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AmDemoComponent } from './ejemplos/am-demo/am-demo.component';
+import { PrincipalComponent } from './ejemplos/principal/principal.component';
+import { PaginaErrorComponent } from './ejemplos/pagina-error/pagina-error.component';
+import { MaterialModule } from './angular-material/material/material.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DirectivasPipesComponent
+    DirectivasPipesComponent,
+    AmDemoComponent,
+    PrincipalComponent,
+    PaginaErrorComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ejemplos/directivas-pipes/directivas-pipes.component.ts
+++ src/app/ejemplos/directivas-pipes/directivas-pipes.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class DirectivasPipesComponent {
 
+  activo = 0;
+  alumnos = [
+    {nombre: "Cesar" },
+    {nombre: "Diego" },
+    {nombre: "José" },
+    {nombre: "Karla" }
+  ];
+
+  cursos = [
+    {nombre: "Desarrollo de aplicaciones web 2", precio: 8000 },
+    {nombre: "Base de datos", precio: 5000 },
+    {nombre: "Desarrollo de aplicaciones móviles 1", precio: 10000 },
+    {nombre: "Desarrollo de aplicaciones móviles 2", precio: 15000 },
+  ];
+
+  fechaActual = new Date();
+
+  mostrarAlumnos(): void {
+    this.activo = 0;
+  }
+
+  mostrarCursos(): void{
+    this.activo = 1;
+  }
+
 }
