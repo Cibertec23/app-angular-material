41dccf966186ae4bb6f8439bd6a1d0837ba6aa21
cuartociclo2023 <cuartocicloidat2023@gmail.com>
2023-11-11 19:28:58 -0500

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DirectivasPipesComponent } from './ejemplos/directivas-pipes/directivas-pipes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { DirectivasPipesComponent } from './ejemplos/directivas-pipes/directivas
     DirectivasPipesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
